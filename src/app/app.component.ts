20f9adb1849312daf34048766fb3b9e49c679e42
Jansmig <jan.smigielski@gmail.com>
2021-02-28 15:05:14 +0100

    password generator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,61 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'PassGenerator';
+
+  useLetters: boolean = false;
+  useNumbers: boolean = false;
+  useSymbols: boolean = false;
+  password: String = '';
+  length = 0;
+
+  onChangeUseLetters(){
+    this.useLetters = !this.useLetters;
+  }
+
+  onChangeUseNumbers(){
+    this.useNumbers = !this.useNumbers;
+  }
+
+  onChangeUseSymbols(){
+    this.useSymbols = !this.useSymbols;
+  }
+
+  onChangeLength(value: string){
+    const parsedValue = parseInt(value);
+    if(!isNaN(parsedValue)){
+      this.length = parsedValue;
+    }
+  }
+
+  onButtonClick(){
+
+    if(!this.useLetters && !this.useNumbers && !this.useSymbols) {
+      this.password = "select some checkbox!";
+      return;
+    }
+
+    const letters = 'qwertyuiopasdfghjklzxcvbnm';
+    const numbers = '1234567890';
+    const symbols = '!@#$%^&*()';
+    let chars = '';
+
+    if(this.useLetters) {
+      chars = chars + letters;
+    };
+    if(this.useNumbers) {
+      chars = chars + numbers;
+    }
+    if(this.useSymbols) {
+      chars = chars + symbols;
+    }
+
+    let generatedPassword = '';
+
+    for (let i = 0; i < this.length; i++) {
+      const index = Math.floor(Math.random() * chars.length);
+      generatedPassword += chars[index]; 
+    }
+    this.password = generatedPassword;
+  }
+
 }
